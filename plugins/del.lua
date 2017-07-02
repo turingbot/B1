b06ab2deb97845571b8964fd4f1ff52600886583
turingbot <tabaghi.m@gmail.com>
2017-07-02 18:42:11 +0430

    Update del.lua

--- plugins/del.lua
+++ plugins/del.lua
@@ -1,4 +1,4 @@
-﻿local function delmsg (i,naji)
+local function delmsg (i,naji)
     msgs = i.msgs 
     for k,v in pairs(naji.messages_) do
         msgs = msgs - 1
@@ -12,12 +12,16 @@
 end
 local function run(msg, matches)
     if matches[1] == 'del' and is_owner(msg) or matches[1] == 'پاک کردن' and is_owner(msg) then
+    if redis:get("delchat:"..msg.to.id) then
+    return "_این کار فقط هر 1ساعت 1بار ممکن میباشد لطفا بعد از یک ساعت دوباره امتحان کنید_"
+    end
         if tostring(msg.to.id):match("^-100") then 
             if tonumber(matches[2]) > 1000 or tonumber(matches[2]) < 1 then
                 return  '🚫 *1000*> _تعداد پیام های قابل پاک سازی در هر دفعه_ >*1* 🚫'
             else
-				tdcli.getChatHistory(msg.to.id, msg.id,0 , 100, delmsg, {msgs=matches[2]})
-				return "`"..matches[2].." `_پیام اخیر با موفقیت پاکسازی شدند_ 🚮"
+        tdcli.getChatHistory(msg.to.id, msg.id,0 , 100, delmsg, {msgs=matches[2]})
+        redis:setex("delchat:"..msg.to.id,3600,true)
+        return ""..matches[2].." _پیام اخیر با موفقیت پاکسازی شدند_ 🚮"
             end
         else
             return '⚠️ _این قابلیت فقط در سوپرگروه ممکن است_ ⚠️'
